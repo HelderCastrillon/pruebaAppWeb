8165bd47ffdf9fe06bb5d462cc360353a8876892
Helder Castrillón <heldersoft@gmail.com>
2019-11-07 12:39:07 -0500

    Add files via upload

---
+++ precache-manifest.71b4fbca26303012f3cc1869eedf038a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06c3bca3c4c554bc4a1e24a409290b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "459341d5dbd11f835f35",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e5a1376d52cd8fe87eae",
+    "url": "/static/js/2.5be0afe2.chunk.js"
+  },
+  {
+    "revision": "459341d5dbd11f835f35",
+    "url": "/static/js/main.05a207c9.chunk.js"
+  },
+  {
+    "revision": "03390053ccc5237f18f6",
+    "url": "/static/js/runtime-main.de883bb6.js"
+  }
+]);
